e897ad7f36f7aa8eb5b4ac85da4e9169ce6ca914
demesis221 <rideouts199@gmail.com>
2025-11-14 20:18:43 +0800

    Add React components and styles

---
+++ src/index.js
@@ -0,0 +1,7 @@
+import React from 'react';
+import ReactDOM from 'react-dom/client';
+import './index.css';
+import LoginForm from './LoginForm';
+
+const root = ReactDOM.createRoot(document.getElementById('root'));
+root.render(<LoginForm />);
