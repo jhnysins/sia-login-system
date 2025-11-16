e42e28316a2cf01fa029e6b35f1f15ef6d085e6d
Krystrella <dicdican.krystalmae@mail.com>
2025-11-16 14:35:26 +0800

    updating firebase

--- src/config/firebase.js
+++ src/config/firebase.js
@@ -1,16 +1,16 @@
 // Import the functions you need from the SDKs you need
 import { initializeApp } from "firebase/app";
 import { getAuth } from "firebase/auth";
-import { getFirestore } from "firebase/firestore"; // Import Firestore
+import { getFirestore } from "firebase/firestore";
 
-// Your web app's Firebase configuration
+
 const firebaseConfig = {
-    apiKey: "AIzaSyCTSe8wfvULQYbsGyXEgrJD1yOgGkTWkhw",
-    authDomain: "login-system-114fe.firebaseapp.com",
-    projectId: "login-system-114fe",
-    storageBucket: "login-system-114fe.firebasestorage.app",
-    messagingSenderId: "132443361016",
-    appId: "1:132443361016:web:8a731a75fc1406b9be750d"
+    apiKey: process.env.REACT_APP_API_KEY,
+    authDomain: process.env.REACT_APP_AUTH_DOMAIN,
+    projectId: process.env.REACT_APP_PROJECT_ID,
+    storageBucket: process.env.REACT_APP_STORAGE_BUCKET,
+    messagingSenderId: process.env.REACT_APP_MESSAGING_SENDER_ID,
+    appId: process.env.REACT_APP_APP_ID
 };
 
 // Initialize Firebase
